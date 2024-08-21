190a781315ec0cf332ce1f9ae750a0a866d4c341
skc-charchit <shreekumarchandancharchit@gmail.com>
2024-08-21 23:46:05 +0530

    Changes has to be done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,44 +7,48 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass
-class DataingestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
-    test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-class Dataingestion:
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
+
+class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataingestionConfig()
-        
+        self.ingestion_config = DataIngestionConfig()
+
     def initiate_data_ingestion(self):
-        logging.info("Data Ingestion has been started")
+        logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebbok\dataset\Encoded_LoanExport.csv')
-            logging.info('Reading the data from the csv file')
-            
+            df = pd.read_csv(r"D:\Work Station\mlproject\notebbok\dataset\Encoded_LoanExport.csv")
+            logging.info('Read the dataset as a dataframe')
+
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-            
+
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info('Train test split initiated')
-
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            
+
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            
+
             logging.info("Ingestion of the data is completed")
-            
+
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
-            
         except Exception as e:
             raise CustomException(e, sys)
-            pass
-        
+
 if __name__ == "__main__":
-    obj = Dataingestion()
-    obj.initiate_data_ingestion()
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
 from src.logger import logging
-# from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -14,17 +13,7 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
-        
+        logging.error(self.error_message)  # Automatically log the error when an exception is raised
+
     def __str__(self):
         return self.error_message
-    
-    
-if __name__ == "__main__":
-    
-    try:
-        a=1/10
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e, sys)
-    
-    
